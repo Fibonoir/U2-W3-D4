d4af807c9d97387679ae9146f89a46f196992b5a
Valentin Goxhaj <valentinogoxhaj@gmail.com>
2024-09-26 17:22:09 +0200

    second commit

--- script.js
+++ script.js
@@ -1,50 +1,24 @@
 const apiKey = '4wl8vqMgJa4ft83XPDKlQ0sFnf1GFol1PqooxmVhTF2zu1tBOJYKApZZ';
-const apiUrl = 'https://api.pexels.com/v1/search?query='
-const query1 = "hamsters";
-const query2 = "anime";
-let arraySrc = [];
+const apiUrl = 'https://api.pexels.com/v1/search?query=';
+const queries = { hamsters: "islands", anime: "sunset" };
 
-const getImages = function(query) {
-    fetch(apiUrl +query, {
-        headers: {
-            Authorization: apiKey
-        }
-    })
-    .then((response) => {
-        if(response.ok){
-            return response.json()
-        } else {
-            throw new Error ("Errore nel recupero delle immagini")
-        }
-    })
-    .then((images) =>{
-        console.log(images)
-        loadImages(images)
-    })
-    .catch((err) => {
-        console.log("ERRORE", err)
-    })
-}
-
-
-const loadImages = (images) => {
-    arraySrc = images.photos.map(image => image.src);
-};
-
-const updateImages = () => {
+const updateImages = (photos) => {
     document.querySelectorAll(".card img").forEach((img, i) => {
-        img.src = arraySrc[i].original;
-        img.style.height = "250px";
-        img.classList.add("object-fit-cover");
+        if (photos[i]) {
+            img.src = photos[i].src.medium;
+            img.style.height = "250px";
+            img.classList.add("object-fit-cover");
+        }
     });
 };
 
-document.getElementById("loadImages").addEventListener("click", () => {
-    const img = getImages(query1);
-    loadImages(img);
-    updateImages();
-});
+const loadImages = (query) => {
+    fetch(apiUrl + query, { headers: { Authorization: apiKey } })
+        .then(response => response.ok ? response.json() : Promise.reject("Error fetching images"))
+        .then(data => updateImages(data.photos))
+        .catch(err => console.log("Error:", err));
+};
 
-document.getElementById("loadOtherImages").addEventListener("click", () => {
-    getImages(query2).then(updateImages);
-});
+// Use one function for both buttons
+document.getElementById("loadImages").addEventListener("click", () => loadImages(queries.hamsters));
+document.getElementById("loadOtherImages").addEventListener("click", () => loadImages(queries.anime));
